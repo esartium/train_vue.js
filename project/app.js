23b56a2396476df74e726d806d8508a3c589e4fe
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2023-11-12 22:06:23 +0300

    1

---
+++ project/app.js
@@ -0,0 +1,79 @@
+const App = {
+    data() {
+        return {
+            myPlaceholder: 'внесите что-то в список',
+            title: 'Список:',
+            inputValue: '',
+            notes: ['note1','note2']
+        }
+    },
+    methods: {
+        inputChangeHandler (event) {
+            console.log('inputChangeHandler', event.target.value);
+            this.inputValue = event.target.value;
+        },
+
+        addNewNote () {
+            if (this.inputValue !== '') {
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+            }
+        },
+
+        deleteNote(idx, $event) {
+            this.notes.splice(idx, 1)
+        },
+
+        // inputKeyPress (event) {
+            // console.log(event.key)
+
+            // if (event.key == 'Enter') {
+            //     this.addNewNote()
+            // }
+        // }
+        // },
+
+        toUpperCase (item) {
+            return item.toUpperCase()
+        },
+
+        multilpliedCount() {
+            console.log('multilpliedCount')
+            return this.notes.length * 100
+        }
+    },
+    computed: {
+        multilpliedCountComputed() {
+            console.log('multilpliedCountComputed')
+            return this.notes.length * 200
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = 'не пиши сюда больше'
+            }
+            console.log('input value changed', value)
+        }
+    }
+
+}
+
+// const app = Vue.createApp(App)
+// app.mount('#app')
+
+Vue.createApp(App).mount('#app')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
